6da5701952b96407edbde20d3e6143433164aa56
Your Name <you@example.com>
2017-07-24 14:17:27 -0400

    added book and category models

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        //Best Practice - models in C# ALWAYS use an ID
+        [Key]//Primary Key
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        //ForeignKeys establishes a relationship with another class
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
